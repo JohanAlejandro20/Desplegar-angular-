9d1762aadb33b1a7b1e0a1ee13c6ce5fbeb5c67a
johan <johanneira902@gmail.com>
2019-11-25 11:19:06 -0500

    stoy arto

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8070);
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname +'/dist/index.html'));
