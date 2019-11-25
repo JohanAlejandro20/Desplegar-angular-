3d54aa627fcf52ac37b1900bf364b0420e114f12
johan <johanneira902@gmail.com>
2019-11-25 11:01:01 -0500

    stoy arto

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname +'/dist/index.html'));
-})
+});
 
 // Start the app by listening on the default Heroku port
 console.log('console listening!');
