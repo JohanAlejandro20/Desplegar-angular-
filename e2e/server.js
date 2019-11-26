a295e8471eeb28a847f5f0e53cd151bf61035fcf
johan <johanneira902@gmail.com>
2019-11-25 19:49:02 -0500

    vamooooosososo

--- e2e/server.js
+++ e2e/server.js
@@ -1,15 +1,16 @@
 //Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
 
+const app = express();
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8070);
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname +'/dist/index.html'));
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-console.log('console listening!');
+app.listen(process.env.PORT || 8080);
